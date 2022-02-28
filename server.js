378dacbc8fa47e26ac687b1b7961c66fcf7ab77d
abhishekdileep <abhishekdileep99@gmail.com>
2022-02-28 15:27:02 +0530

    updated GUI

--- server.js
+++ server.js
@@ -9,16 +9,16 @@ app.post('/upload' , (req,res) => {
     if(req.files === null ){
         return res.status(400).json({msg : 'No file uploaded' })
     }
-     const file = req.files.file;
+    const file = req.files.file;
+    
 
-     file.mv(`./uploads/ls${file.name}` , err=> {
-         if( err){
+    file.mv(`./client/public/uploads/${file.name}` , err=> {
+        if( err){
             console.error(err);
             return res.status(500).send(err);   
-         }
-
-         res.json({ filename : file.name , filePath : `/uploads/${file.name}` , file : file});
-     })
+        }
+        res.json({ filename : file.name , filePath : `./uploads/${file.name}` });
+    })
 
 });
 
